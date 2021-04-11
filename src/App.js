81ad77823b9e2428cf19f4971e0448db6ebfcabf
KorNito <kornelijuspacevicius@gmail.com>
2021-04-11 10:46:40 +0300

    API get request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className='App'>infinite scroll</div>;
+  const [photos, setPhotos] = useState([]);
+  const URL =
+    "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=a515ee85a4403c999e040fb3cc175263&format=json&nojsoncallback=1&api_sig=9e0870db87c66bc6a46e178dfa615596";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setPhotos(data.photos.photo);
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>Images</h1>
+      <div className='PhotoList'>
+        <ul>
+          {photos.map((photo) => (
+            <li key={photo.id}>
+              Title: {photo.title}
+              <img
+                src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+                alt={photo.title}
+              ></img>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
